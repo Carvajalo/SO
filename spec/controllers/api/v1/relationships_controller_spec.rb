064b8572b5b41489050dd8bcbcf2b2e49995da64
adanielm <adanielm@uninorte.edu.co>
2021-04-30 22:44:07 -0500

    Unit and integration tests relationship

--- spec/controllers/api/v1/relationships_controller_spec.rb
+++ spec/controllers/api/v1/relationships_controller_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::RelationshipsController, '#followers' do
-    
+    context "When show followers is successful" do
+
+        let(:user) { create(:user)}
+        let(:follower) { create(:follower, user: user)}
+
+        before do
+            sign_in(user)
+        end
+        
+        it "Should returns HTTP success code" do
+            expect(response).to have_http_status(:success)
+        end
+    end
 end
+
+RSpec.describe Api::V1::RelationshipsController, '#following' do
+    context "When show followers is successful" do
+
+        let(:user) { create(:user)}
+        let(:following) { create(:following, user: user)}
+
+        before do
+            sign_in(user)
+        end
+        
+        it "Should returns HTTP success code" do
+            expect(response).to have_http_status(:success)
+        end
+    end
+end
+
+RSpec.describe Api::V1::RelationshipsController, '#Seguir' do
+
+    context "When to follow a new user is successful" do
+        let(:user) { create(:user)}
+        let(:relationship) { create(:relationship, user: user)}
+
+        before do
+            sign_in(user)
+        end
+        
+        it "Should returns HTTP success code" do
+            expect(response).to have_http_status(:success)
+        end
+    end
+end
+
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -12,4 +12,15 @@ RSpec.describe Relationship, type: :model do
   describe "Validation" do
     it { should validate_presence_of(:followed_id) }
   end
+
+  # Associations Test :
+  # Belongs_to:follower
+  describe "Association" do
+    it { should belong_to(:follower).class_name('User') }
+  end
+
+  # Belongs_to:followed
+  describe "Association" do
+    it { should belong_to(:followed).class_name('User') }
+  end
 end
