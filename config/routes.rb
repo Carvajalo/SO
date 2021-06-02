3cba275649889068b2f634e17d0777699ac5df1c
Carvajalo <78599853+Carvajalo@users.noreply.github.com>
2021-06-02 10:19:41 -0500

    Fixed routes and Paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #root 'tweets#index'
-  root 'conversations#index'
+  root 'tweets#index'
+  #root 'conversations#index'
   #get '/tweets', to: 'tweets#index'
   resources :tweets
   resources :relationships, only: [:create, :destroy]
